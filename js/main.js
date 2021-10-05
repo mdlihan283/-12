368f88fe97ffd76dbb73b102d6e4860714b2202d
Lihan Ahmed <84702623+freelancerlihan@users.noreply.github.com>
2021-10-05 09:42:42 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// =================carosel=============
+$('.sliderWrapper').slick({
+   slidesToShow: 1,
+   slidesToScroll: 1,
+   autoplay: true,
+   autoplaySpeed: 3000,
+   nextArrow:'<button type="button" class="slick-next">Next</button>'
+});
